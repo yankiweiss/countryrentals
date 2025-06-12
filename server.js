fe21a39d80e95e81714e7af5335140b3a95b68f6
yankiweiss <yakovw2706@gmail.com>
2025-06-12 12:30:37 -0500

    update

--- server.js
+++ server.js
@@ -4,30 +4,30 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn.js");
-// const fileUpload = require("express-fileupload");
-// const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 const cloudinary = require("cloudinary");
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
+app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
 
-// app.use(
-//   fileUpload({
-//     limits: { fileSize: 5 * 1024 * 1024 }, // 5MB per file limit
-//   })
-// );
+app.use(
+  fileUpload({
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB per file limit
+  })
+);
 
 
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-// app.use(
-//   cors({
-//     origin: "*", // or '*' for all, or use an array for multiple origins
-//   })
-// );
+app.use(
+  cors({
+    origin: "*", // or '*' for all, or use an array for multiple origins
+  })
+);
 
 
 app.use("/listing", require("./routes/api/listing.js"));
