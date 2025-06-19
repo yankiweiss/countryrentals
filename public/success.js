ddcd5edd03557b5d27da55570362d058bdc3f9e8
yankiweiss <yakovw2706@gmail.com>
2025-06-18 22:47:43 -0400

    update

--- public/success.js
+++ public/success.js
@@ -10,3 +10,12 @@ function launchConfetti() {
     origin: { y: 0.6 }
   });
 }
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const listingId = urlParams.get("listingId");
+
+if (listingId) {
+  console.log("Listing ID from Stripe success redirect:", listingId);
+  
+}
