967e6fa8b2ac213b83b5c7eabea79ea18128bc1d
yankiweiss <yakovw2706@gmail.com>
2025-06-24 18:34:21 -0400

    update

--- public/home-page.js
+++ public/home-page.js
@@ -127,25 +127,25 @@ fetchListings();
 //});
 
 
-async function populateCities() {
-
-  const res = await fetch('http://localhost:3000/search');
-  const data = await res.json();
-  
-  const excludedCities = ['Brooklyn', 'New York', 'Somerset'];
-  const cities = data.filter(city => !excludedCities.includes(city));
-
-  
-
-  const select = document.getElementById('area-select')
-
-  cities.forEach(city => {
-    const option = document.createElement('option');
-    option.value = city;
-    option.textContent = city;
-    select.appendChild(option)
-  })
-  
-}
-
-populateCities();
+//async function populateCities() {
+//
+//  const res = await fetch('http://localhost:3000/search');
+//  const data = await res.json();
+//  
+//  const excludedCities = ['Brooklyn', 'New York', 'Somerset'];
+//  const cities = data.filter(city => !excludedCities.includes(city));
+//
+//  
+//
+//  const select = document.getElementById('area-select')
+//
+//  cities.forEach(city => {
+//    const option = document.createElement('option');
+//    option.value = city;
+//    option.textContent = city;
+//    select.appendChild(option)
+//  })
+//  
+//}
+//
+//populateCities();
